387577298d15a21176b8ad9ea0e14dc847d5af7b
nildoria <sabbir.ahmed87@gmail.com>
2024-02-14 01:09:09 +0600

    env update

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -34,7 +34,7 @@ const useWeather = () => {
       });
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${
-          import.meta.env.VITE_WEATHER_API_KEY
+          import.meta.env.VITE_WEATHER_API
         }&units=metric`
       );
       if (!response.ok) {
